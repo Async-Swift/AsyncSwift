23d8ecf72d110a2c5e337d65051b94f74d73984d
E-know <eknow25@gmail.com>
2022-09-08 23:27:55 +0900

    [Modify] Add Comment

--- AsyncSwift/Views/StampView.swift
+++ AsyncSwift/Views/StampView.swift
@@ -38,7 +38,8 @@ struct StampView: View {
     //MARK: Flip Card Function
     func flipCard () {
         isFlipped.toggle()
-        if isFlipped {
+        
+        if isFlipped { // 카드 회전 연속을 위해서 if문 분리
             withAnimation(.linear(duration: durationAndDelay)) {
                 backDegree = 90
             }
@@ -60,14 +61,14 @@ private extension StampView {
     @ViewBuilder
     var stampBack: some View {
         Image("Seminar002StampBack")
-            .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.2), radius: 20, x: 0, y: 4)
+            .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.2), radius: 20, x: 0, y: 4) //TODO: 디자인 수정 사항이 있습니다.
             .rotation3DEffect(Angle(degrees: frontDegree), axis: (x: 0, y: 1, z: 0))
     }
 
     @ViewBuilder
     var stampFront: some View {
         Image("Seminar002StampFront")
-            .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.2), radius: 20, x: 0, y: 4)
+            .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.2), radius: 20, x: 0, y: 4) //TODO: 디자인 수정 사항이 있습니다.
             .rotation3DEffect(Angle(degrees: backDegree), axis: (x: 0, y: 1, z: 0))
     }
 
