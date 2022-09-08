ad8cc7f6e27a380b6aa60db74f948cca650a6e74
insub4067 <vb15@kakao.com>
2022-09-08 23:44:45 +0900

    [Refactor]  SessionView 뷰 분리
    - View를 view property로 분리하였습니다

--- AsyncSwift/Views/SessionView.swift
+++ AsyncSwift/Views/SessionView.swift
@@ -20,48 +20,59 @@ struct SessionView: View {
             Divider()
                 .padding(.top, 10)
                 .padding(.bottom, 4)
-            VStack(alignment: .leading) {
-                Text(data.title)
-                    .fontWeight(.semibold)
-                    .font(.system(size: 20))
-                    .padding(.vertical, 24)
 
-                VStack(alignment: .leading, spacing: 8) {
-                    ForEach(data.description, id: \.self) { paragraph in
-                        Text(paragraph.content)
-                    }
-                }
-                .padding(.bottom, 80)
-            }
-            .padding(.horizontal, 24)
-            VStack(alignment: .leading, spacing: 4) {
-                HStack {
-                    Circle()
-                        .frame(width: 80, height: 80, alignment: .leading)
-                        .padding(.vertical, 24)
-                    Spacer()
-                }
-                VStack(alignment: .leading, spacing: 2) {
-                    Text("\(data.speaker.name) 님")
-                        .fontWeight(.semibold)
-                        .font(.system(size: 17))
-                    Text(data.speaker.role)
-                        .fontWeight(.regular)
-                        .font(.system(size: 11))
-                }
-                Text(data.speaker.description)
-                    .fontWeight(.regular)
-                    .font(.system(size: 13))
-                    .padding(.bottom, 60)
-
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .padding(.horizontal, 32)
-            .background(.gray.opacity(0.1))
-
+            sessionDetail
+            speakerDetail
         }
         .navigationTitle("Session")
 
+    }
+}
+
+private extension SessionView {
+
+    var sessionDetail: some View {
+        VStack(alignment: .leading) {
+            Text(data.title)
+                .fontWeight(.semibold)
+                .font(.system(size: 20))
+                .padding(.vertical, 24)
+
+            VStack(alignment: .leading, spacing: 8) {
+                ForEach(data.description, id: \.self) { paragraph in
+                    Text(paragraph.content)
+                }
+            }
+            .padding(.bottom, 80)
+        }
+        .padding(.horizontal, 24)
+    }
+
+    var speakerDetail: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            HStack {
+                Circle()
+                    .frame(width: 80, height: 80, alignment: .leading)
+                    .padding(.vertical, 24)
+                Spacer()
+            }
+            VStack(alignment: .leading, spacing: 2) {
+                Text("\(data.speaker.name) 님")
+                    .fontWeight(.semibold)
+                    .font(.system(size: 17))
+                Text(data.speaker.role)
+                    .fontWeight(.regular)
+                    .font(.system(size: 11))
+            }
+            Text(data.speaker.description)
+                .fontWeight(.regular)
+                .font(.system(size: 13))
+                .padding(.bottom, 60)
+
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(.horizontal, 32)
+        .background(.gray.opacity(0.1))
     }
 }
 
