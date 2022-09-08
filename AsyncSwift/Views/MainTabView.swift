6e48488afad9cf316fb1df002f685cd7444bc30e
E-know <eknow25@gmail.com>
2022-09-08 16:21:36 +0900

    [Feat] Set TabView selection

--- AsyncSwift/Views/MainTabView.swift
+++ AsyncSwift/Views/MainTabView.swift
@@ -8,21 +8,26 @@
 import SwiftUI
 
 struct MainTabView: View {
+	@EnvironmentObject var appData: AppData
     var body: some View {
-        TabView {
+		TabView(selection: $appData.currentTab) {
             EventView()
                 .tabItem {
                     Label("Event", systemImage: "calendar")
                 }
+				.tag(Tab.event)
             
             TicketingView()
                 .tabItem {
                     Label("Ticketing", systemImage: "banknote")
                 }
+				.tag(Tab.ticketing)
+			
             StampView()
                 .tabItem {
                     Label("Stamp", systemImage: "checkmark.square")
                 }
+				.tag(Tab.stamp)
         }
     }
 }
