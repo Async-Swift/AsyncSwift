57a2fd79a410f5279917e4e2b6162a1acdce5f27
unnnyong <unnnyong@gmail.com>
2022-09-09 02:21:03 +0900

    [Feat] Ticket 버튼을 탭하면 Ticketing 가능한 WebView로 이동

--- AsyncSwift/Views/TicketingView.swift
+++ AsyncSwift/Views/TicketingView.swift
@@ -8,15 +8,20 @@
 import SwiftUI
 
 struct TicketingView: View {
+    private let upcomingEventURL = "https://www.eventbrite.com/e/asyncswift-seminar-002-tickets-408509251167"
+
 	var body: some View {
 		NavigationView {
 			ScrollView {
 				VStack(spacing: 30) {
-					Image("Seminar002Ticket")
-						.resizable()
-						.aspectRatio(contentMode: .fit)
-						.shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.15), radius: 20, x: 0, y: 4)
-
+                    NavigationLink {
+                        WebView(url: upcomingEventURL)
+                    } label: {
+                        Image("Seminar002Ticket")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.15), radius: 20, x: 0, y: 4)
+                    }
 					Image("UpcomingConference001")
 						.resizable()
 						.aspectRatio(contentMode: .fit)
