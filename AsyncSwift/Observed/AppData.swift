58caec2a55e34c51b585581d9a42883c6ae4d09c
Inho Choi <55151796+E-know@users.noreply.github.com>
2022-09-08 18:33:24 +0900

    Apply suggestions from code review
    Co-authored-by: Eunyeong Kim <unnnyong@gmail.com>

--- AsyncSwift/Observed/AppData.swift
+++ AsyncSwift/Observed/AppData.swift
@@ -5,18 +5,17 @@
 //  Created by Inho Choi on 2022/09/08.
 //
 
-import Foundation
 import UIKit
 
-class AppData: ObservableObject {
+final class AppData: ObservableObject {
 	@Published var currentTab: Tab = .event
 	
 }
 
 
 enum Tab: String {
-	case event = "event"
-	case ticketing = "ticketing"
-	case stamp = "stamp"
+	case event
+	case ticketing
+	case stamp
 }
 
