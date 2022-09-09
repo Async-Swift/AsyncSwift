39b0b0ed53dac036dc9fb0a19dba98f20ee5e846
E-know <eknow25@gmail.com>
2022-09-10 01:54:04 +0900

    [Feat] Add function for universal links
    - Add checkLink func in AppData

--- AsyncSwift/Observed/AppData.swift
+++ AsyncSwift/Observed/AppData.swift
@@ -9,15 +9,28 @@ import SwiftUI
 import UIKit
 
 final class AppData: ObservableObject {
-	@Published var currentTab: Tab = .event // Universal Link로 앱진입시 StampView 전환을 위한 변수
+    @Published var currentTab: Tab = .event // Universal Link로 앱진입시 StampView 전환을 위한 변수
     var scannedSeminarQR = true // Universal Link로 진입시 QR코드 스캔 여부
+
+    func checkLink(url: URL) -> Bool {
+        guard let host = URLComponents(url: url, resolvingAgainstBaseURL: true)?.host else { return false }
+
+        switch host {
+        case Tab.stamp.rawValue:
+            scannedSeminarQR = true
+            currentTab = .stamp
+        default:
+            currentTab = .ticketing
+        }
+        return true
+    }
 }
 
 
 enum Tab: String, CaseIterable {
-     case event
-     case ticketing
-     case stamp
+    case event
+    case ticketing
+    case stamp
 
     var title: String {
         rawValue
@@ -39,4 +52,4 @@ enum Tab: String, CaseIterable {
         case .stamp: StampView()
         }
     }
- }
+}
