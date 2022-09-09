dc840a4eab9876ead842397d0181131ad561f4b6
unnnyong <unnnyong@gmail.com>
2022-09-09 19:08:20 +0900

    [Refactor] TicketingView를 IVOPattern에 맞게 수정

--- AsyncSwift/Views/TicketingView.swift
+++ AsyncSwift/Views/TicketingView.swift
@@ -8,26 +8,28 @@
 import SwiftUI
 
 struct TicketingView: View {
-    private let upcomingEventURL = "https://www.eventbrite.com/e/asyncswift-seminar-002-tickets-408509251167"
+    @StateObject private var observed = Observed()
 
 	var body: some View {
 		NavigationView {
 			ScrollView {
 				VStack(spacing: 30) {
-                    NavigationLink {
-                        WebView(url: upcomingEventURL)
-                    } label: {
-                        Image("Seminar002Ticket")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.15), radius: 20, x: 0, y: 4)
-                    }
-					Image("UpcomingConference001")
-						.resizable()
-						.aspectRatio(contentMode: .fit)
-				}
-					.aspectRatio(contentMode: .fit)
-					.padding(30)
+                    NavigationLink(
+                        isActive: $observed.isActivatedWebViewNavigationLink) {
+                            WebView(url: observed.upcomingEventURL)
+                        } label: {
+                            Image("Seminar002Ticket")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.15), radius: 20, x: 0, y: 4)
+                        }
+
+                    Image("UpcomingConference001")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+                .aspectRatio(contentMode: .fit)
+                .padding(30)
 
 			}
 				.navigationTitle("Ticketing")
