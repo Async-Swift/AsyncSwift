5496c0f9068ec86793512cc7385a3e14bc21c023
E-know <eknow25@gmail.com>
2022-09-15 01:05:59 +0900

    [Feat] URL Parsing

--- AsyncSwift/Observed/AppData.swift
+++ AsyncSwift/Observed/AppData.swift
@@ -13,15 +13,30 @@ final class AppData: ObservableObject {
     var scannedSeminarQR = true // Universal Link로 진입시 QR코드 스캔 여부
 
     func checkLink(url: URL) -> Bool {
+        // URL Example = https://www.asyncswift.info?seminar=seminar002&tab=ticketing
+        // URL Example = https://www.asyncswift.info?seminar=seminar002&tab=event
         guard let host = URLComponents(url: url, resolvingAgainstBaseURL: true)?.host else { return false }
 
-        switch host {
+        var queries: [String: String] = [:]
+        for item in URLComponents(url: url, resolvingAgainstBaseURL: true)?.queryItems ?? [] {
+            queries[item.name] = item.value
+        }
+        
+        //TODO: seminar002도 나중에 JSON으로 관리하면 앱 업데이트 할 필요 없이 간편하게 수정 할 수 있을것 같습니다.
+        if queries["seminar"] != "seminar002" {
+            return false
+        }
+
+        switch queries["tab"] {
         case Tab.stamp.rawValue:
             scannedSeminarQR = true
             currentTab = .stamp
+        case Tab.event.rawValue:
+            currentTab = .event
         default:
-            currentTab = .ticketing
+            return false
         }
+
         return true
     }
 }
