10cc9c35b3c78419c93e5c441cfd3ec4d5d1c8e5
E-know <eknow25@gmail.com>
2022-10-08 17:00:04 +0900

    [Refactor] Tab 모델 이전

--- AsyncSwift/Observed/MainTabView+Observed.swift
+++ AsyncSwift/Observed/MainTabView+Observed.swift
@@ -86,30 +86,3 @@ extension MainTabView {
         }
     }
 }
-
-enum Tab: String, CaseIterable {
-     case event = "Event"
-     case ticketing = "Ticketing"
-     case stamp = "Stamp"
-
-    var title: String {
-        rawValue
-    }
-
-    var systemImageName: String {
-        switch self {
-        case .event: return "calendar"
-        case .ticketing: return "banknote"
-        case .stamp: return "checkmark.square"
-        }
-    }
-
-    @ViewBuilder
-    var view: some View {
-        switch self {
-        case .event: EventView()
-        case .ticketing: TicketingView()
-        case .stamp: StampView()
-        }
-    }
-}
