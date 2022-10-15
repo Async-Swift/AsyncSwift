592cecf4ea17372fec66e031157f6e883a3db2ea
Inho Choi <eknow25@gmail.com>
2022-10-15 11:25:23 +0900

    [Fix] 필요없는 print문 삭제

--- AsyncSwift/Extensions/String+.swift
+++ AsyncSwift/Extensions/String+.swift
@@ -10,7 +10,6 @@ import Foundation
 extension String {
     func convertToStringArray() -> [String]? {
         guard let stringData = self.data(using: .utf8) else {
-            print("Convert String Data -> [String] is fail | String: \(self)")
             return nil
         }
         
@@ -18,7 +17,6 @@ extension String {
         do {
             result = try JSONDecoder().decode([String].self, from: stringData)
         } catch {
-            print("Convert String Data -> [String] is fail | String: \(self)")
             return nil
         }
         return result
