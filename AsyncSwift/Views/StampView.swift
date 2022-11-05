c674df389e11c7e1fc9a52d0a1d5971c76b66077
Inho Choi <eknow25@kakao.com>
2022-11-05 13:04:23 +0900

    [Feat] :sparkles: Card 없는 뷰 추가

--- AsyncSwift/Views/StampView.swift
+++ AsyncSwift/Views/StampView.swift
@@ -11,30 +11,51 @@ struct StampView: View {
     @StateObject var observed = Observed()
     
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            ScrollViewReader { value in
-                ZStack {
-                    ForEach(0..<observed.cards.count, id: \.self) { index in
-                        observed.cards[observed.events[index]]?.currentImage?
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .offset(y: observed.getCardOffsetY(index: index))
-                            .onTapGesture {
-                                observed.didCardTapped(index: index)
+        Group {
+            if observed.cards.isEmpty {
+                emptyCardView
+                    .padding(36)
+            } else {
+                ScrollView(showsIndicators: false) {
+                    ScrollViewReader { value in
+                        ZStack {
+                            ForEach(0..<observed.cards.count, id: \.self) { index in
+                                observed.cards[observed.events[index]]?.currentImage?
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .offset(y: observed.getCardOffsetY(index: index))
+                                    .onTapGesture {
+                                        observed.didCardTapped(index: index)
+                                    }
                             }
+                        }
+                        .offset(y: UIScreen.main.bounds.height / 2)
                     }
+                    Spacer(minLength: observed.isExpand ? UIScreen.main.bounds.height : 0)
                 }
-                .offset(y: UIScreen.main.bounds.height / 2)
+                .padding()
             }
-            Spacer(minLength: observed.isExpand ? UIScreen.main.bounds.height : 0)
-        }
-        .padding()
+        } // Group
         .onOpenURL{ url in
             Task {
                 await observed.openByLink(url: url)
             }
         }
     } // body
+}
+
+private extension StampView {
+    var emptyCardView: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 30)
+                .strokeBorder(Color(red: 0.78, green: 0.78, blue: 0.8), style: StrokeStyle(lineWidth: 2, dash: [10]))
+            
+            Text("아직 참여한 행사가 없습니다.")
+                .foregroundColor(.gray)
+        }
+    }
+    
+    
 }
 
 struct StampView_Previews: PreviewProvider {
